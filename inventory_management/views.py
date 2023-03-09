bb44746ddf5360f0e2e24d994e20ffee8faca6ca
james-leonard <merrymerry1320@gmail.com>
2023-03-09 23:16:16 +0100

    added logo

--- inventory_management/views.py
+++ inventory_management/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 from django.db.models import Sum
 
 import datetime
